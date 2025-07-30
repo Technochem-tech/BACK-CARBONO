36ce29ea158c6acd3acf4fe86b2e89b18c8c96cb
Marlonjms <marlonjose150404@gmail.com>
2025-07-30 18:40:00 -0300

    Ativa Swagger na produção

--- WebApplicationCarbono/Program.cs
+++ WebApplicationCarbono/Program.cs
@@ -51,11 +51,11 @@ builder.Services.AddScoped<IBinanceServico, BinanceServico>();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
+
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseMiddleware<ExceptionMiddleware>();
 
